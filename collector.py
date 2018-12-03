4f0a874e30cb127b3d419d94ff4b6a07d23947d5
JJPavlik <jjpavlik@gmail.com>
2018-12-03 21:17:08 +0000

    Forgot a few things

--- collector.py
+++ collector.py
@@ -35,6 +35,14 @@ def main():
     terminate = False
     debug = False
 
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h", ["help", "debug"])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(err)  # will print something like "option -a not recognized"
+        #usage()
+        sys.exit(2)
+
     for o, a in opts:
         if o == "-v":
             verbose = True
