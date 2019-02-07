f8d387f2b6d39b737b770f80dc7e6484d4b6b9bf
Juan <jjpavlik@gmail.com>
2019-02-07 20:58:54 +0000

    changed logging to see what the heck is going on

--- device.py
+++ device.py
@@ -94,9 +94,10 @@ class Arduino(Device):
         message.append(WRITE)                    #B2
         message.append(sensor_id)			#B3
 
+        logging.deubg("About to write to sensor " + self.available_sensors[sensor_id]['name'] + " type " + str(self.available_sensors[sensor_id]['type']))
+
         if self.available_sensors[sensor_id]['type'] == 32: #Sensor is actually a LCD display
             message.append(5 + len(data[0]) + 1 + len(data[1])) #B4 SIZE
-            logging.deubg("About to write to sensor " + self.available_sensors[sensor_id]['name'] + " type " + str(self.available_sensors[sensor_id]['type']))
             #Copy the letters one by one on the message payload.
             aux = list(data[0])
             for i in aux:
