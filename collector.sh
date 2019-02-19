270603e1f97a32ace6bc3a8b86afee54ef3edfd7
Juan <jjpavlik@gmail.com>
2019-02-19 22:20:37 +0000

    Working on systemd scripts to start both collector.py and pusher.py automatically. collector.sh and pusher.sh will be the starting/stopping scripts. Now need to come up with an install.sh

---
+++ collector.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function start {
+  source /opt/homemetrics/bin/activate
+  exec python collector.py -f 30
+}
+
+function stop {
+
+}
+
+function status {
+
+}
+
+cd /opt/homemetrics
+
+case "$1" in
+  start)
+      start
+      ;;
+  stop)
+      stop
+      ;;
+  status)
+      status
+      ;;
+  restart)
+      stop
+      start
+      ;;
+  *)
+      echo $"Usage: $0 {start|stop|restart|status}"
+      exit 1
+esac
