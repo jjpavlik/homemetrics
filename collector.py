486612311e8fc35186735449308549e51da7454e
JJPavlik <jjpavlik@gmail.com>
2019-11-11 22:45:51 +0000

    Added missing parameters to basicConfig() since it was not working as expected

--- collector.py
+++ collector.py
@@ -204,12 +204,12 @@ def main():
         if path.exists("debug"):
             if not debug: #If debug file was just created, enable DEBUG
                 debug = True
-                logging.basicConfig(level=logging.DEBUG)
+                logging.basicConfig(filename="pusher.log", format=FORMAT, level=logging.DEBUG)
                 logging.info("DEBUG logging ENABLED")
         else:
             if debug: #If debug file was just deleted disable it (regardless of --debug start)
                 debug = False
-                logging.basicConfig(level=logging.INFO)
+                logging.basicConfig(filename="pusher.log", format=FORMAT, level=logging.INFO)
                 logging.info("DEBUG logging DISABLED (back to INFO)")
 
         back_to_sleep_for = (frequency - ((time.time() - starttime)%frequency))
